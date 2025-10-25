d86262c9eec22415a9614d4a2280ddc68ef0fb0e
Junyoung Kim <junyoungkim@Junyoungui-MacBookPro.local>
2025-10-26 02:14:25 +0900

    fix vite base path

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import SForecastApp from "./App";
-import "./index.css";
+import "./App.css";   // ✅ index.css가 아니라 App.css 임 (중요)
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
