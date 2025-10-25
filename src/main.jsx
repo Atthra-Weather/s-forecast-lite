6c5feaa649cc457f4f32be7c156534754b2eee98
Junyoung Kim <junyoungkim@Junyoungui-MacBookPro.local>
2025-10-26 02:37:57 +0900

    Move working PWA version to lite repo

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import SForecastApp from "./App";
-import "./App.css";   // ✅ index.css가 아니라 App.css 임 (중요)
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SForecastApp />
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
